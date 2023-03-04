1c93ea7bd1442700b8197b33bb656e60d5448ebb
chit <cxl203@student.bham.ac.uk>
2023-03-04 00:01:55 +0000

    I think I successfully

--- djangosite/polls/views.py
+++ djangosite/polls/views.py
@@ -1,6 +1,23 @@
+import json
+
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    names = ("bob", "dan", "jack", "lizzy", "susan")
+
+    items = []
+    for i in range(100):
+        items.append({
+            "name": random.choice(names),
+            "age": random.randint(20, 80),
+            "url": "https://example.com",
+        })
+
+    context = {}
+    context["items"] = items
+    context["items_json"] = json.dumps(items)
+
+    return render(request, 'index.html', context)

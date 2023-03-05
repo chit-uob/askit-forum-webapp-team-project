66ae3b2422c0e1d9ef246a9ca75b88cecebf1832
chit <cxl203@student.bham.ac.uk>
2023-03-05 13:51:08 +0000

    test postgresql deploy

--- djangosite/djangosite/settings.py
+++ djangosite/djangosite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'djangosite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "tpdb",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
 

c54f64acf3acce86b11ac02cc2ec1168a7f4ff65
chit <cxl203@student.bham.ac.uk>
2023-03-05 14:13:36 +0000

    fix host name and run correct docker

--- djangosite/djangosite/settings.py
+++ djangosite/djangosite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "tpdb",  # set in docker-compose.yml
+        "HOST": "tppostgres",  # set in docker-compose.yml
         "PORT": 5432,  # default postgres port
     }
 }

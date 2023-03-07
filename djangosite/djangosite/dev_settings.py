7a4dd6d83e4c2c76ad203d44dde2fbcdc5e1ecd9
chit <cxl203@student.bham.ac.uk>
2023-03-07 14:54:10 +0000

    set up home page routing

--- djangosite/djangosite/dev_settings.py
+++ djangosite/djangosite/dev_settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'home_page.apps.HomePageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

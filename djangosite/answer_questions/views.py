0d001731ca05f0c1730db01488b67751d92e21c6
chit <cxl203@student.bham.ac.uk>
2023-03-08 14:20:55 +0000

    try fetch question, if not exist say it

--- djangosite/answer_questions/views.py
+++ djangosite/answer_questions/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from home_page.models import Question, Answer
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the answer questions")
 
 def view_question(request, question_id):
-    return render(request, 'answer_questions/view_question.html', {'question_id': question_id})
+    try:
+        question = Question.objects.get(id=question_id)
+        title = question.title
+    except Question.DoesNotExist:
+        title = "Question does not exist"
+    return render(request, 'answer_questions/view_question.html', {'question_id': question_id, 'title': title})

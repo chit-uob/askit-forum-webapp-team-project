0b3f003de80d63e5f322de9847ad0150469400d5
chit <cxl203@student.bham.ac.uk>
2023-03-08 13:52:57 +0000

    have page to view question

--- djangosite/answer_questions/views.py
+++ djangosite/answer_questions/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the answer questions")
+
+def view_question(request, question_id):
+    return render(request, 'answer_questions/view_question.html', {'question_id': question_id})

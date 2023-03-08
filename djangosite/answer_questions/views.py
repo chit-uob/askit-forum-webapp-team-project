dc72952d4bb67fdc15d963e93eb128f5b546955a
chit <cxl203@student.bham.ac.uk>
2023-03-08 14:34:17 +0000

    return json response of the object

--- djangosite/answer_questions/views.py
+++ djangosite/answer_questions/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from home_page.models import Question, Answer
 
@@ -6,10 +6,29 @@ from home_page.models import Question, Answer
 def index(request):
     return HttpResponse("Hello, world. You're at the answer questions")
 
+
+def check_upvote_or_downvote(question):
+    return "upvote"
+
+
 def view_question(request, question_id):
+    print(Question.objects.all())
+    context = {}
     try:
         question = Question.objects.get(id=question_id)
-        title = question.title
+        context['title'] = question.title
+        context['author'] = question.author
+        context['module'] = question.module.title
+        context['explanation'] = question.explanation
+        context['tried_what'] = question.tried_what
+        context['summary'] = question.summary
+        context['pub_date'] = question.pub_date
+        context['status'] = question.status
+        # context['tags'] = question.tags
+        context['score'] = question.score
+        context['views'] = question.views
+        context['upvote_or_downvote'] = check_upvote_or_downvote(question)
     except Question.DoesNotExist:
         title = "Question does not exist"
-    return render(request, 'answer_questions/view_question.html', {'question_id': question_id, 'title': title})
+    # return render(request, 'answer_questions/view_question.html', context)
+    return JsonResponse(context)

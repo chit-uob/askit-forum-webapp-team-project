e4d7f69a865cf2c020fd6009afa6dde254109d53
chit <cxl203@student.bham.ac.uk>
2023-03-10 09:21:05 +0000

    change admin path to api/admin

--- askit/askit/urls.py
+++ askit/askit/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/',include('djoser.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken'))
 ]

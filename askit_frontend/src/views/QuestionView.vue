58eb81ca4ee0d71cc61b68185a2bd5c1e269b70c
chit <cxl203@student.bham.ac.uk>
2023-03-10 12:35:03 +0000

    as question view

--- askit_frontend/src/views/QuestionView.vue
+++ askit_frontend/src/views/QuestionView.vue
@@ -1,12 +1,62 @@
 <template>
   <h1>ab</h1>
   <h1>Question {{ $route.params }}</h1>
+
+
+  <div class="max-w-6xl mx-auto bg-white rounded-lg shadow-lg overflow-hidden" style="width:90%;">
+    <div class="p-4 flex items-center">
+      <div class="flex flex-col items-center">
+        <button class="bg-green-500 hover:bg-green-600 text-white font-bold py-1 px-2 rounded mr-5">
+          <i class="fa fa-arrow-up"></i>
+        </button>
+        <br>
+        <button class="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded mr-5">
+          <i class="fa fa-arrow-down"></i>
+        </button>
+      </div>
+      <div>
+<!--        <h1 class="text-3xl font-bold ">{{ title }}</h1>-->
+        <br>
+<!--        <p class="mb-2">{{ explanation }},{{ author }}, {{ module }}, {{ tried_what }}, {{ summary }}, {{ pub_date }},-->
+<!--          {{ status }}, {{ score }}, {{ views }}, {{ upvote_or_downvote }}, {{ tags }}</p>-->
+
+      </div>
+      <div class="p-4">
+<!--        <p class="mb-4">{{ author }}, {{ module }}, {{ explanation }}, {{ tried_what }}, {{ summary }}, {{ pub_date }},-->
+<!--          {{ status }}, {{ score }}, {{ views }}, {{ upvote_or_downvote }}, {{ tags }}</p>-->
+        <form action="submit_answer/" method="post" class="mt-4">
+<!--          {% csrf_token %}-->
+          <div class="flex items-center">
+            <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                   type="text" name="content">
+            <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 ml-4 rounded" type="submit">
+              Submit answer
+            </button>
+          </div>
+          <br><br>
+        </form>
+        <h1 class="text-2xl font-bold mb-4">Answers:</h1>
+<!--        {% for answer in answer_list %}-->
+        <div class="p-4 mb-4 bg-white rounded-lg shadow">
+<!--          <p class="text-lg">{{ answer.content }}</p>-->
+<!--          <p class="text-gray-600 text-sm mt-2">Author: {{ answer.author }} | Published: {{ answer.pub_date }} | Score:-->
+<!--            {{ answer.score }}</p>-->
+<!--          {% if answer.is_solution %}-->
+          <p class="text-green-500 text-sm font-semibold mt-2">Solution</p>
+<!--          {% endif %}-->
+        </div>
+<!--        {% endfor %}-->
+      </div>
+    </div>
+  </div>
+
+
 </template>
 
 <script>
 export default {
   name: "QuestionView",
-}
+};
 </script>
 
 <style scoped>

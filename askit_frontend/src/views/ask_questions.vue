2ec0a96f75b18a6b24d5d1386cc8744b270ad58f
Aditya Agarwal <adityaagarwal191@gmail.com>
2023-03-10 16:23:39 +0000

    added button and removed ref

--- askit_frontend/src/views/ask_questions.vue
+++ askit_frontend/src/views/ask_questions.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Ref } from 'vue';
+// import { Ref } from 'vue';
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
 
@@ -8,4 +8,6 @@ import '@vueup/vue-quill/dist/vue-quill.snow.css'
 <template>
   <QuillEditor theme="snow" toolbar="full">
   </QuillEditor>
+  <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Blue</button>
+
 </template>

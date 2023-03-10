c46e3b9af05627ce2b38c6081e235212b40e6cde
Aymon Hussain <axh1220@student.bham.ac.uk>
2023-03-10 20:30:25 +0000

    Revert "Merge branch 'main' into 'Aymon_Aliyu_module_page'"
    This reverts merge request !13

--- askit/answer_questions/urls.py
+++ askit/answer_questions/urls.py
@@ -5,8 +5,8 @@ app_name = 'answer_questions'
 urlpatterns = [
 #     path('', views.index, name='index'),
     path('<int:question_id>/', views.view_question, name='view_question'),
-    path('<int:question_id>/submit_answer/', views.submit_answer, name='submit_answer'),
+#     path('<int:question_id>/submit_answer/', views.submit_answer, name='submit_answer'),
 #     path('<int:question_id>/upvote/', views.upvote, name='upvote'),
 #     path('<int:question_id>/downvote/', views.downvote, name='downvote'),
-    path('create_question/', views.create_question, name='create_question'),
+#     path('create_question/', views.create_question, name='create_question'),
 ]

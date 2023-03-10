97b0e732aba90c0813e8a55db25fb6e0f4966c9e
chit <cxl203@student.bham.ac.uk>
2023-03-10 11:14:22 +0000

    try json response

--- askit/answer_questions/views.py
+++ askit/answer_questions/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def view_question(request, question_id):
+    context = {}
+    try:
+        question = Question.objects.get(id=question_id)
+        question.views += 1
+        question.save()
+        context['question_id'] = question.id
+        context['title'] = question.title
+        context['author'] = question.author
+        context['module'] = question.module.title
+        context['explanation'] = question.explanation
+        context['tried_what'] = question.tried_what
+        context['summary'] = question.summary
+        context['pub_date'] = question.pub_date
+        context['status'] = question.status
+        context['tags'] = list(question.tags.all())
+        context['score'] = question.score
+        context['views'] = question.views
+        context['upvote_or_downvote'] = check_upvote_or_downvote(question)
+
+        answer_query_result = Answer.objects.filter(question=question)
+        context['answer_list'] = []
+        for answer in answer_query_result:
+            answer_dict = {}
+            answer_dict['author'] = answer.author
+            answer_dict['content'] = answer.content
+            answer_dict['pub_date'] = answer.pub_date
+            answer_dict['score'] = answer.score
+            answer_dict['is_solution'] = answer.is_solution
+            context['answer_list'].append(answer_dict)
+    except Question.DoesNotExist:
+        context['question_id'] = context['title'] = context['author'] = context['module'] = context['explanation'] = context['tried_what'] = context['summary'] = context['pub_date'] = context['status'] = context['score'] = context['views'] = context['upvote_or_downvote'] = "Question does not exist"
+    return return JsonResponse(context)
--- askit/askit/urls.py
+++ askit/askit/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
-    path('api/v1/', include('djoser.urls.authtoken'))
+    path('api/v1/', include('djoser.urls.authtoken')),
+    path('api/question/', include('answer_questions.urls')),
 ]

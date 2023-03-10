b28e7ee0c8e3ef5bafb13b6c2a21ff07aa94868e
chit <cxl203@student.bham.ac.uk>
2023-03-10 23:48:21 +0000

    fix title explain same line

--- askit_frontend/src/views/ask_questions.vue
+++ askit_frontend/src/views/ask_questions.vue
@@ -12,7 +12,7 @@ import '@vueup/vue-quill/dist/vue-quill.snow.css'
 <textarea id="message" rows="1" class="block p-2.5 w-1/2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your title"></textarea>
 
   <label for="large-input" class="block mb-2 text-lg font-medium text-green-700 dark:text-black">Explain your problem</label>
-<div w-75>
+<div class="w-1/2">
   <QuillEditor theme="snow" toolbar="full">
   </QuillEditor>
 </div>

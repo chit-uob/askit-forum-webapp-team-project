3a817a8a74f8896ad72a19e74eda181b7b1d432a
Abbas Mandasorwala <aam119@student.bham.ac.uk>
2023-03-11 19:04:34 +0000

    re-arranged answers part format

--- askit_frontend/src/views/QuestionView.vue
+++ askit_frontend/src/views/QuestionView.vue
@@ -57,20 +57,19 @@
         <button class="bg-green-500 hover:bg-green-600 text-white font-bold py-1 px-2 rounded mr-2">
           <i class="fa fa-arrow-up"></i>
         </button>
-        <span class="text-gray-600 text-lg font-bold mr-2">{{ question.score }}</span>
+        <span class="text-gray-600 text-lg font-bold mr-2">{{ answer.score }}</span>
         <button class="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded mr-2">
           <i class="fa fa-arrow-down"></i>
         </button>
-      </div>
-          <div>
-            <p class="text-lg">{{ answer.content }}</p>
-          <p class="text-gray-600 text-sm mt-2">Author: abc123@student.bham.ac.uk {{ answer.author }} | Published: {{ answer.pub_date }} | Score:
-            {{ answer.score }}</p>
-          <p v-if="answer.is_solution" class="text-green-500 text-sm font-semibold mt-2">Solution</p>
-
           </div>
-
-
+          <div class="flex-grow">
+            <div class="flex justify-between">
+              <p class="text-gray-600 text-sm">Author: abc123@student.bham.ac.uk {{ answer.author }}  </p>
+              <p class="text-gray-600 text-sm text-right ">Published: {{ answer.pub_date }}</p>
+            </div>
+            <p v-if="answer.is_solution" class="text-green-500 text-sm font-semibold mt-2">Solution</p>
+            <p class="text-lg">{{ answer.content }}</p>
+          </div>
         </div>
       </div>
     </div>

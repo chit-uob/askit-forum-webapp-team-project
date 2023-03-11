4706b31bea1c0a50caae15badee2ce6dbd413f02
Aymon Hussain <axh1220@student.bham.ac.uk>
2023-03-11 22:37:16 +0000

    added front end for module page, no css

--- askit_frontend/src/views/ModuleView.vue
+++ askit_frontend/src/views/ModuleView.vue
@@ -1,5 +1,52 @@
 <template>
-    <div class="about">
-      <h1>This is the Module page</h1>
+    <div>
+      <div>
+        <h1>{{ $route.params.mod.toUpperCase() }}</h1>
+        <button>Ask Question</button>
+      </div>
+      <div>
+        <h2>Most recent questions</h2>
+          <div v-for="question in questions" :key="question.id">
+            <p>{{ question.title }}</p>
+            <p>Asked by {{ question.author }}</p>
+            <p>{{ question.tags }}</p>
+          </div>
+      </div>
     </div>
+
+
   </template>
+
+<script>
+import axiosClient from "@/views/axiosClient";
+
+export default {
+  name: "ModuleView",
+  data() {
+    return {
+      
+      questions: [
+        { id: 1, title: 'Learn HTML?', author: 'Aymon', tags: ['HTML', 'help'] },
+        { id: 2, title: 'Learn JavaScript?', author: 'Aliyu', tags: ['JS', 'help'] },
+        { id: 3, title: 'Learn Vue?', author: 'Adi', tags: ['Vue', 'help'] }
+      ]
+    };
+  },
+  mounted() {
+    axiosClient({
+      method: "get",
+      url: `/module/${this.$route.params.mod}/`,
+    })
+        .then((response) => {
+          this.questions = response.data;
+          console.log(response.data)
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  },
+  methods: {
+    
+  }
+};
+</script>

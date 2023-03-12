f5aa57b7b3debeedca9d75e3f0a9df890af07467
Aditya Agarwal <adityaagarwal191@gmail.com>
2023-03-12 15:30:45 +0000

    formatting done

--- askit_frontend/src/views/ask_questions.vue
+++ askit_frontend/src/views/ask_questions.vue
@@ -6,45 +6,47 @@ import '@vueup/vue-quill/dist/vue-quill.snow.css'
 </script>
 
 <template>
-<body class="bg-amber-200">
-    <div class="absolute left-2/3 ml-7 w-0.5 h-screen bg-gray-600"></div>
-</body>
-<!-- <p class= ".p-52">rules</p> -->
-<label for="message" class="block mb-2 -ml-40 text-lg font-medium text-gray-900 dark:text-black">Title</label>
 
-  
+<div class="absolute left-2/3 ml-7 mt-0.5 w-0.5 h-screen bg-gray-600"></div>
+
+<!-- <p class= ".p-52">rules</p> -->
+<!-- <label for="message" class="block -ml-40 text-lg font-medium text-gray-900 dark:text-black">Title</label> -->
+
+<div>
 <label for="message" class="block mb-2 text-lg font-medium text-gray-900 dark:text-black">Title</label>
 <textarea id="message" rows="1" class="block p-2.5 w-2/3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your title"></textarea>
+</div>
 
-  <label for="large-input" class="block mb-2 text-lg font-medium text-green-700 dark:text-black">Explain your problem</label>
+
+<label for="large-input" class="block mb-2 text-lg font-medium text-green-700 dark:text-black">Explain your problem</label>
 <div class="w-2/3">
-  <QuillEditor theme="snow" toolbar="full">
+  <QuillEditor id ="large-input" theme="snow" toolbar="full">
   </QuillEditor>
 </div>
-  <label for="large-input" class="block mb-2 text-lg font-medium text-green-700 dark:text-black">What have you already tried?</label>
 
-  
 
-  <div class="w-2/3">
-  <QuillEditor theme="snow" toolbar="full">
-  </QuillEditor>
+<label for="large-input" class="block mb-2 text-lg font-medium text-green-700 dark:text-black">What have you already tried?</label>
+<div class="w-2/3">
+<QuillEditor id ="large-input" theme="snow" toolbar="full">
+</QuillEditor>
 </div>
+
 <!--summary-->
-  <div class="mb-6">
-  <label for="large-input" class="block mb-2 text-lg font-medium text-green-700 dark:text-black">Summary (optional)</label>
-  <textarea id="message" rows="1" class="block p-2.5 w-2/3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type or auto-generate summary"></textarea>
+<div class>
+<label for="message" class="block mb-2 text-lg font-medium text-green-700 dark:text-black">Summary (optional)</label>
+<textarea id="message" rows="1" class="block p-2.5 w-2/3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type or auto-generate summary"></textarea>
+<button type="button" class=" mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Auto-generate summary</button>
 </div>
-<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Auto-generate summary</button>
 
 
 
 
 <!--tags-->
 <div class="mb-6">
-  <label for="large-input" class="block mb-2 text-lg font-medium text-green-700 dark:text-black">Tags</label>
-  <textarea id="message" rows="1" class="block p-2.5 w-2/3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type or auto-generate tags"></textarea>
+<label for="message" class="block mb-2 text-lg font-medium text-green-700 dark:text-black">Tags</label>
+<textarea id="message" rows="1" class="block p-2.5 w-2/3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type or auto-generate tags"></textarea>
+<button type="button" class="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Auto-generate tags</button>
 </div>
 
-<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Auto-generate tags</button>
 
 </template>

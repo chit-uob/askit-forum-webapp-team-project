b8402e53d4dcdec712563b1863a5fc1674d14f8b
Aliyu Suleiman <aliyusuleiman098@gmail.com>
2023-03-12 15:51:08 +0000

    Started layout for module page

--- askit_frontend/src/views/ModuleView.vue
+++ askit_frontend/src/views/ModuleView.vue
@@ -1,52 +1,38 @@
 <template>
-    <div>
-      <div>
-        <h1>{{ $route.params.mod.toUpperCase() }}</h1>
-        <button>Ask Question</button>
+    <div class="w-screen h-screen flex">
+      <div class="w-[150px] h-full bg-cyan-100">
+        Side Bar
       </div>
-      <div>
-        <h2>Most recent questions</h2>
-          <div v-for="question in questions" :key="question.id">
-            <p>{{ question.title }}</p>
-            <p>Asked by {{ question.author }}</p>
-            <p>{{ question.tags }}</p>
+      <div class="w-full h-screen ">
+        <div class="p-10 inline-flex justify-between w-full">
+          <h1 class="text-5xl font-bold">Team Project</h1>
+          <button type="submit"
+                  class="text-white  bg-blue-600 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">
+            Ask Question
+          </button>
+        </div>
+        <h2 class="pl-10 text-2xl font-bold"> Top Questions</h2>
+        <div class="pt-2 pl-10 pr-10 flex w-full h-screen">
+          <div class="card inline-flex w-full h-20 bg-cyan-200 rounded-2xl shadow outline-black">
+            <div class=" w-1/12 p-3 flex flex-col justify-evenly">
+              <p>votes</p>
+              <p>answers</p>
+              <p>views</p>
+            </div>
+            <div class=" w-10/12 p-3 flex flex-col justify-evenly">
+              <h3>Some Question that has been asked?</h3>
+              <p>Asked by:</p>
+              <p>[Tag][Tag]</p>
+            </div>
+            <div class=" w-1/12 p-3 flex flex-col justify-evenly">
+              <h3>DATE</h3>
+            </div>
           </div>
+
+        </div>
       </div>
-    </div>
-
-
+      <div class="w-[400px] h-full bg-gray-400">
+        -- after m2
+      </div>
+      </div>
   </template>
-
-<script>
-import axiosClient from "@/views/axiosClient";
-
-export default {
-  name: "ModuleView",
-  data() {
-    return {
-      
-      questions: [
-        { id: 1, title: 'Learn HTML?', author: 'Aymon', tags: ['HTML', 'help'] },
-        { id: 2, title: 'Learn JavaScript?', author: 'Aliyu', tags: ['JS', 'help'] },
-        { id: 3, title: 'Learn Vue?', author: 'Adi', tags: ['Vue', 'help'] }
-      ]
-    };
-  },
-  mounted() {
-    axiosClient({
-      method: "get",
-      url: `/module/${this.$route.params.mod}/`,
-    })
-        .then((response) => {
-          this.questions = response.data;
-          console.log(response.data)
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-  },
-  methods: {
-    
-  }
-};
-</script>

b20f4ef46e7fe69774c574e86357528fbff97744
Aymon Hussain <axh1220@student.bham.ac.uk>
2023-03-13 23:44:10 +0000

    Added modules to side bar and cleaned layout

--- askit_frontend/src/views/ModuleView.vue
+++ askit_frontend/src/views/ModuleView.vue
@@ -13,29 +13,32 @@
         </button>
         </a>
       </div>
-      <h2 class="pl-10 text-2xl font-bold"> Top Questions</h2>
+      <h2 class="pl-10 text-2xl font-bold">All Questions</h2>
       <div v-for="question in questions" :key="question.id">
         <a class="pt-2 pl-10 pr-10 flex w-full" :href="`/question/${question.id}`">
           <div class="card inline-flex w-full h-20 bg-cyan-200 rounded-2xl shadow outline-black">
-            <div class=" w-1/12 p-3 flex flex-col justify-evenly">
-              <p>votes: {{ question.score }}</p>
-              <p>answers: {{ question.num_answers }}</p>
-              <p>views {{ question.views }}</p>
+            <div class=" w-[100px] p-3 flex flex-col justify-evenly">
+              <p class="text-xs text-right">{{ question.score }} votes</p>
+              <p class="text-xs text-right">{{ question.num_answers }} answers</p>
+              <p class="text-xs text-right">{{ question.views }} views</p>
             </div>
-            <div class=" w-10/12 p-3 flex flex-col justify-evenly">
+            <div class=" w-8/12 p-3 flex flex-col justify-evenly">
               <h3>{{ question.title }}</h3>
-              <p>Asked by: {{ question.author }}</p>
-              <p>{{ question.tags }}</p>
+              <p>Asked by [User]{{ question.author }}</p>
+              <p>{{ question.tags.substring(10,question.tags.length-1) }}</p>
             </div>
-            <div class=" w-1/12 p-3 flex flex-col justify-evenly">
+            <div class=" w-[80px] p-3 flex flex-col justify-evenly">
               <h3>{{ question.pub_date }}</h3>
             </div>
           </div>
         </a>
       </div>
     </div>
-    <div class="w-[400px] h-full bg-gray-400">
-      -- after m2
+    <div class="w-[400px] h-full bg-gray-200 p-3">
+      <h1 class=" font-bold text-lg">Modules</h1>
+        <a href="/module/OSSP" class=" text-sky-600"><p>OSSP</p></a>
+        <a href="/module/TP" class="text-sky-600"><p>TP</p></a>
+      
     </div>
   </div>
 </template>

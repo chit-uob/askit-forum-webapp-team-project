f5895140994471b8e80d61391ebf717ef1c7e43a
Juni Katsu <jxk010@student.bham.ac.uk>
2023-03-13 14:10:05 +0000

    small changes

--- askit/ask_questions/urls.py
+++ askit/ask_questions/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'answer_questions'
+app_name = 'ask_questions'
 urlpatterns = [
 #     path('', views.index, name='index'),
-#     path('<int:question_id>/', views.view_question, name='view_question'),
+    path('ask/<int:question_id>/', views.display_questions, name='display_question'),
 #     path('<int:question_id>/submit_question/', views.submit_question, name='submit_question'),
 #     path('<int:question_id>/upvote/', views.upvote, name='upvote'),
 #     path('<int:question_id>/downvote/', views.downvote, name='downvote'),
--- askit/ask_questions/views.py
+++ askit/ask_questions/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 import json
 from django.http import JsonResponse
 # Create your views here.
-from home_page.models import Question, Module
+from home_page.models import Question, Module, Tag, Answer
 from django.views.decorators.csrf import csrf_exempt
+import requests
 
 @csrf_exempt
 def submit_question(request):
@@ -27,4 +28,40 @@ def submit_question(request):
         question_dict['views'] = Question.views
 
         return JsonResponse(question_dict)
+
+def display_questions(request, question_id):
+    dis_question = {}
+    try:
+        question = Question.objects.get(id=question_id)
+        question.views += 1
+        question.save()
+        dis_question['question_id'] = question.id
+        dis_question['title'] = question.title
+        dis_question['author'] = question.author
+        dis_question['module'] = question.module.title
+        dis_question['explanation'] = question.explanation
+        dis_question['tried_what'] = question.tried_what
+        dis_question['summary'] = question.summary
+        dis_question['pub_date'] = question.pub_date
+        dis_question['status'] = question.status
+        dis_question['tags'] = str(question.tags.all())
+        dis_question['score'] = question.score
+        dis_question['views'] = question.views
+        # context['upvote_or_downvote'] = check_upvote_or_downvote(question)
+
+        # answer_query_result = Answer.objects.filter(question=question)
+        # context['answer_list'] = []
+        # for answer in answer_query_result:
+        #     answer_dict = {}
+        #     answer_dict['author'] = answer.author
+        #     answer_dict['content'] = answer.content
+        #     answer_dict['pub_date'] = answer.pub_date
+        #     answer_dict['score'] = answer.score
+        #     answer_dict['is_solution'] = answer.is_solution
+        #     context['answer_list'].append(answer_dict)
+    except Question.DoesNotExist:
+        dis_question['question_id'] = dis_question['title'] = dis_question['author'] = dis_question['module'] = dis_question['explanation'] = \
+        dis_question['tried_what'] = dis_question['summary'] = dis_question['pub_date'] = dis_question['status'] = dis_question['score'] = \
+        dis_question['views'] = dis_question['upvote_or_downvote'] = "Question does not exist"
+    return JsonResponse(dis_question)
 

1a55789cd6c82f70c29315d9031072f3b125eb32
Aymon Hussain <axh1220@student.bham.ac.uk>
2023-03-14 17:39:56 +0000

    formatted privacy policy

--- askit_frontend/src/views/PrivacyView.vue
+++ askit_frontend/src/views/PrivacyView.vue
@@ -1,9 +1,9 @@
 <template>
-<div class="p-10">
+<div class="px-[100px] py-[20px]">
 <p class="text-2xl text-center font-bold p-3">Team 55 Privacy Policy</p>
 <p>This privacy policy will explain how our organization uses the personal data we collect from you when you use our website.</p>
 
-<p class="font-bold">Topics: </p>
+<p class="font-bold text-lg">Topics: </p>
 <ul class="indent-4">
 <li>- What data do we collect? </li>
 <li>- How do we collect your data?</li>
@@ -21,86 +21,98 @@
 <li>- How to contact the appropriate authorities</li>
 </ul>
 <br>
-<p>What data do we collect? <br>
-Our Website collects the following data: <br>
-Personal identification information (Name, email address,) <br>
-passwords<br>
-enrolled courses<br>
-enrolled modules<br>
-questions asked<br>
-questions answered<br>
-questions upvoted and downvoted<br>
-questions viewed<br>
-date and time of activity (questions asked and answered)<br>
-uploaded pictures</p>
-
-<p>How do we collect your data? <br>
-You directly provide us with most of the data we collect. We collect data and process data when you: <br>
-Register online. <br>
-Voluntarily ask a question or provide answers on any of the courses/module pages. <br>
-Use or view our website via your browser's cookies. <br>
-Upvote/Downvote on any question or answer.</p>
-
-<p>Our Team may also receive your data indirectly from the following sources: <br>
-When TA or Module lead adds you to a course/module.</p>
-
-<p>How will we use your data? <br>
-Our Team collects your data so that we can: <br>
-Manage your account. <br>
-Enhance the user experience by filtering popular or helpful questions and answers.</p>
-
-<p>How do we store your data? <br>
-Our Team securely stores your data on a University of Birmingham VM which is secured by a RSA key based SSH.</p>
-
-<p>Our Team will keep all data listed above for 3 years excluding your name, password and email . Once this time period has expired, we will delete your data by removing them from the database. <br>
-What are your data protection rights? <br>
-Our Team would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p>
-
-<p>The right to access - You have the right to request our Team for copies of your personal data.</p>
-
-<p>The right to rectification - You have the right to request our Team to correct any information you believe is inaccurate. You also have the right to request our Team to complete information you believe is incomplete.</p>
-
-<p>The right to erasure - You have the right to request our Team to erase your personal data.</p>
-
-<p>The right to restrict processing - You have the right to request that our Team restrict the processing of your personal data, under certain conditions.</p>
-
-<p>The right to object to processing - You have the right to object to our Team's processing of your personal data, under certain conditions.</p>
-
-<p>The right to data portability - You have the right to request that our Team transfer the data that we have collected from you to another organization, or directly to you, under certain conditions.</p>
-
+<p class="font-bold text-lg">What data do we collect?</p>
+<p>Our Website collects the following data: </p>
+<ul class="indent-4">
+<li>- Personal identification information (Name, email address,) </li>
+<li>- passwords</li>
+<li>- enrolled courses</li>
+<li>- enrolled modules</li>
+<li>- questions asked</li>
+<li>- questions answered</li>
+<li>- questions upvoted and downvoted</li>
+<li>- questions viewed</li>
+<li>- date and time of activity (questions asked and answered)</li>
+<li>- uploaded pictures</li>
+</ul>
+<br>
+<p class="font-bold text-lg">How do we collect your data? </p>
+<p>You directly provide us with most of the data we collect. We collect data and process data when you: </p>
+<ul class="indent-4">
+<li>- Register online.</li>
+<li>- Voluntarily ask a question or provide answers on any of the courses/module pages. </li>
+<li>- Use or view our website via your browser's cookies. </li>
+<li>- Upvote/Downvote on any question or answer.</li>
+</ul>
+<br>
+<p>Our Team may also receive your data indirectly from the following sources:</p>
+<ul class="indent-4">
+<li>- When TA or Module lead adds you to a course/module.</li>
+</ul>
+<br>
+<p class="font-bold text-lg">How will we use your data? </p>
+<p>Our Team collects your data so that we can: </p>
+<ul class="indent-4">
+<li>- Manage your account. </li>
+<li>- Enhance the user experience by filtering popular or helpful questions and answers.</li>
+</ul>
+<p class="font-bold text-lg">How do we store your data? </p>
+<p>Our Team securely stores your data on a University of Birmingham VM which is secured by a RSA key based SSH.</p>
+<br>
+<p>Our Team will keep all data listed above for 3 years excluding your name, password and email . Once this time period has expired, we will delete your data by removing them from the database. </p>
+<p class="font-bold text-lg">What are your data protection rights? </p>
+<p>Our Team would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p>
+<br>
+<p><span class="font-bold">The right to access</span> - You have the right to request our Team for copies of your personal data.</p>
+<br>
+<p><span class="font-bold">The right to rectification</span> - You have the right to request our Team to correct any information you believe is inaccurate. You also have the right to request our Team to complete information you believe is incomplete.</p>
+<br>
+<p><span class="font-bold">The right to erasure</span> - You have the right to request our Team to erase your personal data.</p>
+<br>
+<p><span class="font-bold">The right to restrict processing</span> - You have the right to request that our Team restrict the processing of your personal data, under certain conditions.</p>
+<br>
+<p><span class="font-bold">The right to object to processing</span> - You have the right to object to our Team's processing of your personal data, under certain conditions.</p>
+<br>
+<p><span class="font-bold">The right to data portability</span> - You have the right to request that our Team transfer the data that we have collected from you to another organization, or directly to you, under certain conditions.</p>
+<br>
 <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us at our email: axh1220@student.bham.ac.uk</p>
-
-<p>What are cookies? <br>
-Cookies are text files placed on your computer to collect standard Internet log information and visitor behavior information. When you visit our websites, we may collect information from you automatically through cookies or similar technology.</p>
-
-<p>For further information, visit allaboutcookies.org.</p>
-
-<p>How do we use cookies? <br>
-Our Team uses cookies in a range of ways to improve your experience on our website, including: <br>
- Keeping you signed in <br>
- Keeping any settings changes you made</p>
-
-<p>What types of cookies do we use? <br>
-There are a number of different types of cookies, however, our website uses: <br>
-Functionality - Our Team uses these cookies so that we recognize you on our website and remember your previously selected preferences. These could include what language you prefer and location you are in. A mix of first-party and third-party cookies are used. <br>
-Advertising - Our Team uses these cookies to collect information about your visit to our website, the content you viewed, the links you followed and information about your browser, device, and your IP address.</p>
-
-<p>How to manage cookies <br>
-You can set your browser not to accept cookies, and the above website tells you how to remove cookies from your browser. However, in a few cases, some of our website features may not function as a result.</p>
-
-<p>Privacy policies of other websites <br>
- Our website may contain links to other websites. Our privacy policy applies only to our website, so if you click on a link to another website, you should read their privacy policy.</p>
-
-<p>Changes to our privacy policy <br>
-Our Team keeps its privacy policy under regular review and places any updates on this web page. This privacy policy was last updated on 11 March 2023.</p>
-
-<p>How to contact us <br>
-If you have any questions about Our Team's privacy policy, the data we hold on you, or you would like to exercise one of your data protection rights, please do not hesitate to contact us.</p>
-
-<p>Email us at: axh1220@student.bham.ac.uk<br>
-How to contact the appropriate authority <br>
-Should you wish to report a complaint or if you feel that Our Team has not addressed your concern in a satisfactory manner, you may contact the University of Birmingham IT Help Desk. <br>
-Web: itservicedesk.bham.ac.uk <br>
+<br>
+<p class="font-bold text-lg">What are cookies? </p>
+<p>Cookies are text files placed on your computer to collect standard Internet log information and visitor behavior information. When you visit our websites, we may collect information from you automatically through cookies or similar technology.</p>
+<br>
+<p>For further information, visit <span href="allaboutcookies.org">allaboutcookies.org</span>.</p>
+<br>
+<p class="font-bold text-lg">How do we use cookies? </p>
+<p>Our Team uses cookies in a range of ways to improve your experience on our website, including: </p>
+<ul class="indent-4"> 
+<li>- Keeping you signed in </li>
+<li>- Keeping any settings changes you made</li>
+</ul>
+<br>
+<p class="font-bold text-lg">What types of cookies do we use? </p>
+<p>There are a number of different types of cookies, however, our website uses: </p>
+<ul class="indent-4">
+<li>- Functionality - Our Team uses these cookies so that we recognize you on our website and remember your previously selected preferences. These could include what language you prefer and location you are in. A mix of first-party and third-party cookies are used. </li>
+<li>- Advertising - Our Team uses these cookies to collect information about your visit to our website, the content you viewed, the links you followed and information about your browser, device, and your IP address.</li>
+</ul>
+<br>
+<p class="font-bold text-lg">How to manage cookies </p>
+<p>You can set your browser not to accept cookies, and the above website tells you how to remove cookies from your browser. However, in a few cases, some of our website features may not function as a result.</p>
+<br>
+<p class="font-bold text-lg">Privacy policies of other websites </p>
+<p>Our website may contain links to other websites. Our privacy policy applies only to our website, so if you click on a link to another website, you should read their privacy policy.</p>
+<br>
+<p class="font-bold text-lg">Changes to our privacy policy </p>
+<p>Our Team keeps its privacy policy under regular review and places any updates on this web page. This privacy policy was last updated on 11 March 2023.</p>
+<br>
+<p class="font-bold text-lg">How to contact us </p>
+<p>If you have any questions about Our Team's privacy policy, the data we hold on you, or you would like to exercise one of your data protection rights, please do not hesitate to contact us.</p>
+<br>
+<p>Email us at: axh1220@student.bham.ac.uk</p>
+<br>
+<p class="font-bold text-lg">How to contact the appropriate authority </p>
+<p>Should you wish to report a complaint or if you feel that Our Team has not addressed your concern in a satisfactory manner, you may contact the University of Birmingham IT Help Desk. <br>
+Web: itservicedesk.bham.ac.uk<br>
 Tel: +44 (0)121 414 7171<br>
 </p>
 </div>

b132662103a28ba976a9d0413c72abeef776bebd
Juni Katsu <jxk010@student.bham.ac.uk>
2023-03-14 13:56:17 +0000

    fixing some error at urls.py

--- askit/ask_questions/urls.py
+++ askit/ask_questions/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # path('<int:question_id>/', views.display_questions, name='displayQuestions'),
     path('summary/', views.summry_api, name='summaryApi'),
     path('tagging/', views.tag_api, name='tagApi'),
-    path('/module/<str:mod>/', views.submit_question, name='submitQuestions'),
+    path('module/<str:mod>/', views.submit_question, name='submitQuestions'),
 ]

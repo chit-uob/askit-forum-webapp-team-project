79a7cfd17a00454c28ac23feb13151de772e2805
chit <cxl203@student.bham.ac.uk>
2023-03-17 11:55:00 +0000

    added search app

--- askit/askit/settings.py
+++ askit/askit/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'home_page.apps.HomePageConfig',
     'ask_questions.apps.AskQuestionsConfig',
     'answer_questions.apps.AnswerQuestionsConfig',
+    'questions.apps.QuestionsConfig',
 ]
 
 MIDDLEWARE = [

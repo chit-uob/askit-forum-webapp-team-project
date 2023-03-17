f11c22ae4376a7897c967963a957e2bf2a514102
chit <cxl203@student.bham.ac.uk>
2023-03-17 12:45:04 +0000

    basic search all and return response console logged

--- askit/search/views.py
+++ askit/search/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from home_page.models import Question, Module, Tag, Answer
+
+
+@csrf_exempt
+def search_all_modules(request):
+    if request.method == 'POST':
+        post_data = json.loads(request.body)
+        search_term = post_data['searchTerm']
+        search_results = Question.objects.filter(title__icontains=search_term)
+        question_array = []
+        for question in search_results:
+            context = {'id': question.id,
+                       'title': question.title,
+                       'author': question.author,
+                       'pub_date': question.pub_date,
+                       'tags': []
+                       }
+            for tag_name in question.tags.all():
+                context['tags'].append(str(tag_name))
+            context['score'] = question.score
+            context['views'] = question.views
+            context['num_answers'] = Answer.objects.filter(question=question).count()
+            question_array.append(context)
+        return JsonResponse({"search_results": question_array}, safe=False)

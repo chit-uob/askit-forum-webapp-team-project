bce7897834df013bdbd5fee0ad54add385c3f1d5
Aditya Agarwal <adityaagarwal191@gmail.com>
2023-03-19 19:20:12 +0000

    added json instead of text field

--- askit_frontend/src/views/QuestionView.vue
+++ askit_frontend/src/views/QuestionView.vue
@@ -47,7 +47,7 @@
         <form @submit.prevent="addAnswer" class="w-5/6">
           <br>
           <div>
-            <QuillEditor theme="snow" toolbar="full" name="content" v-model:content="answerInput" contentType="text">
+            <QuillEditor theme="snow" toolbar="full" name="content" v-model:content="answerInput" contentType="json">
             </QuillEditor>
           </div>
           <br>

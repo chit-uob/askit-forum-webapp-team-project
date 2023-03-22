315baf471f8d6481545076ea331c7afc0d3ebdf5
Abbas Mandasorwala <aam119@student.bham.ac.uk>
2023-03-22 01:09:41 +0000

    Added some css changes to signUp page

--- askit_frontend/src/views/SignUpView.vue
+++ askit_frontend/src/views/SignUpView.vue
@@ -1,16 +1,38 @@
 <template>
-    <div >
-        <h1>Sign up</h1>
-        <form @submit.prevent="submitForm">
-            <label for="username">Username</label>
-            <input type="email" name="username" v-model="username" placeholder="email"  class="border-2">
-            <label for="password">Password</label>
-            <input type="password" name="password" v-model="password" placeholder="password" class="border-2">
-            <button type="submit"  class="bg-blue-300">Sign up</button>
-        </form>
+  <div class="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+    <div class="flex items-center flex-wrap mb-8">
+      <h1 class="text-6xl font-bold text-center font-sans text-black mr-8">ASK.IT</h1>
+      <form @submit.prevent="submitForm" class="bg-white p-8 rounded-lg shadow-md w-96">
+        <h1 class="text-3xl font-bold mb-8 text-center">Sign up</h1>
+        <div class="mb-4">
+          <label for="username" class="block text-gray-700 font-bold mb-2">Username</label>
+          <input type="email" name="username" v-model="username" placeholder="Email" class="border-2 p-2 w-full rounded text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-400">
+        </div>
+        <div class="mb-4">
+          <label for="password" class="block text-gray-700 font-bold mb-2">Password</label>
+          <input type="password" name="password" v-model="password" placeholder="Password" class="border-2 p-2 w-full rounded text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-400">
+        </div>
+        <button type="submit" class="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:bg-blue-600">Sign up</button>
+      </form>
     </div>
+  </div>
 </template>
 
+
+
+
+<!--<template>-->
+<!--    <div >-->
+<!--        <h1>Sign up</h1>-->
+<!--        <form @submit.prevent="submitForm">-->
+<!--            <label for="username">Username</label>-->
+<!--            <input type="email" name="username" v-model="username" placeholder="email"  class="border-2">-->
+<!--            <label for="password">Password</label>-->
+<!--            <input type="password" name="password" v-model="password" placeholder="password" class="border-2">-->
+<!--            <button type="submit"  class="bg-blue-300">Sign up</button>-->
+<!--        </form>-->
+<!--    </div>-->
+<!--</template>-->
 <script>
 import axiosClient from './axiosClient';
 

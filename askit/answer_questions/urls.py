cbc28ef48c0d50f2c6c310360906f7e40c186aab
chit <cxl203@student.bham.ac.uk>
2023-03-24 13:32:11 +0000

    add path for comment

--- askit/answer_questions/urls.py
+++ askit/answer_questions/urls.py
@@ -5,6 +5,7 @@ app_name = 'answer_questions'
 urlpatterns = [
     path('<int:question_id>/', views.view_question, name='view_question'),
     path('<int:question_id>/submit_answer/', views.submit_answer, name='submit_answer'),
+    path('<int:question_id>/submit_comment/', views.submit_comment, name='submit_comment'),
     path('<int:question_id>/upvote/', views.upvote, name='upvote'),
     path('<int:question_id>/downvote/', views.downvote, name='downvote'),
     path('<int:question_id>/answer/<int:answer_id>/upvote/', views.upvote_answer, name='upvote_answer'),

d3e9fc40aa29ad113368fbf2d6f20e74dbc3a9c7
chit <cxl203@student.bham.ac.uk>
2023-03-26 23:35:40 +0100

    remove dark mode style

--- askit_frontend/src/views/CreateModule.vue
+++ askit_frontend/src/views/CreateModule.vue
@@ -2,18 +2,18 @@
   <main class="flex-1 relative z-0 overflow-y-auto focus:outline-none crypto_bg">
     <div class="py-6">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-        <h1 class="block mb-2 text-3xl font-semibold text-gray-900 dark:text-white">Create Course</h1>
+        <h1 class="block mb-2 text-3xl font-semibold text-gray-900">Create Course</h1>
       </div>
       <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
         <div class="mb-6">
-          <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Course
+          <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">Course
             Name</label>
           <input type="text" id="default-input" v-model="moduleTitle"
-                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
         </div>
-        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Course description</label>
+        <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Course description</label>
         <textarea id="message" rows="16" v-model="moduleExplanation"
-                  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                   placeholder="Description here..."></textarea>
 
         <button v-on:click="addModule" type="button"

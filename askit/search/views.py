ef088f556045fd5ca7c73ebe64e03703c578414c
Aditya Agarwal <adityaagarwal191@gmail.com>
2023-03-27 15:29:09 +0400

    advanced title and content search working

--- askit/search/views.py
+++ askit/search/views.py
@@ -52,4 +52,34 @@ def search_questions(request):
 
 def advanced_search(request):
     # todo: implement advanced search
-    return JsonResponse([], safe=False)
+
+    if request.method == 'GET':
+        adv_result = Question.objects.all() 
+        if 'titleContains' in request.GET:
+            titleContains = request.GET['titleContains']
+            adv_result = Question.objects.filter(title__icontains=titleContains)
+
+        if 'contentContains' in request.GET:
+            contentContains = request.GET['contentContains']
+            adv_result = Question.objects.filter(explanation__icontains=contentContains)
+
+        # if 'containTags' in request.GET:
+        #     containTags = request.GET['containTags']
+        #     adv_result = Question.objects.filter(tags__icontains=containTags)
+        
+        question_array = []
+        for question in adv_result:
+            context = {'id': question.id,
+                    'title': question.title,
+                    'author': question.author,
+                    'pub_date': question.pub_date,
+                    'tags': [],
+                    'module': question.module.title
+                    }
+            for tag_name in question.tags.all():
+                context['tags'].append(str(tag_name))
+            context['score'] = question.score
+            context['views'] = question.views
+            context['num_answers'] = Answer.objects.filter(question=question).count()
+            question_array.append(context)
+        return JsonResponse(question_array, safe=False)

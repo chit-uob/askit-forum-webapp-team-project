ddc84e1efec1351f283493aeef179eb44290344d
chit <cxl203@student.bham.ac.uk>
2023-03-27 21:58:41 +0100

    advance search correct if

--- askit/search/views.py
+++ askit/search/views.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from home_page.models import Question, Module, Tag, Answer, User, Notifications, Comment
+from home_page.models import Question, Module, Tag, Answer
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 
 
 @api_view(['GET'])
@@ -16,17 +17,17 @@ def search_questions(request):
         if 'module' in request.GET:
             module = request.GET['module']
             result_search = Module.objects.get(title=module)
-            search_results = Question.objects.filter( title__icontains=search_term).filter(module=result_search)
+            search_results = Question.objects.filter(title__icontains=search_term).filter(module=result_search)
 
             question_array = []
             for question in search_results:
                 context = {'id': question.id,
-                        'title': question.title,
-                        'author': getattr(question.author, 'username', 'Anonymous'),
-                        'pub_date': question.pub_date,
-                        'tags': [],
-                        'module': question.module.title
-                        }
+                           'title': question.title,
+                           'author': getattr(question.author, 'username', 'Anonymous'),
+                           'pub_date': question.pub_date,
+                           'tags': [],
+                           'module': question.module.title
+                           }
                 for tag_name in question.tags.all():
                     context['tags'].append(str(tag_name))
                 context['score'] = question.score
@@ -34,7 +35,7 @@ def search_questions(request):
                 context['num_answers'] = Answer.objects.filter(question=question).count()
                 question_array.append(context)
             return JsonResponse(question_array, safe=False)
-            
+
         else:
             search_results = Question.objects.filter(title__icontains=search_term)
             question_array = []
@@ -59,42 +60,40 @@ def search_questions(request):
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def advanced_search(request):
-    # todo: implement advanced search
-
     if request.method == 'GET':
-        adv_result = Question.objects.all() 
-        if 'titleContains' in request.GET:
+        adv_result = Question.objects.all()
+        if request.GET['titleContains']:
             titleContains = request.GET['titleContains']
             adv_result = adv_result.filter(title__icontains=titleContains)
 
-        if 'contentContains' in request.GET:
+        if request.GET['contentContains']:
             contentContains = request.GET['contentContains']
             adv_result = adv_result.filter(explanation__icontains=contentContains)
 
-        if 'course' in request.GET:
+        if request.GET['course']:
             course = request.GET['course']
             adv_module_search = Module.objects.get(title=course)
             adv_result = adv_result.filter(module=adv_module_search)
 
-        if 'containTags' in request.GET:
+        if request.GET['containTags']:
             containTags = request.GET['containTags']
             tag_result = Tag.objects.filter(tag_name=containTags)
             adv_result = adv_result.filter(tags__in=tag_result)
 
-        if 'byUser' in request.GET:
+        if request.GET['byUser']:
             byUser = request.GET['byUser']
-            user_result = User.objects.get(name=byUser)
+            user_result = User.objects.get(email=byUser)
             adv_result = adv_result.filter(author=user_result)
-        
+
         question_array = []
         for question in adv_result:
             context = {'id': question.id,
-                    'title': question.title,
-                    'author': getattr(question.author, 'username', 'Anonymous'),
-                    'pub_date': question.pub_date,
-                    'tags': [],
-                    'module': question.module.title
-                    }
+                       'title': question.title,
+                       'author': getattr(question.author, 'username', 'Anonymous'),
+                       'pub_date': question.pub_date,
+                       'tags': [],
+                       'module': question.module.title
+                       }
             for tag_name in question.tags.all():
                 context['tags'].append(str(tag_name))
             context['score'] = question.score

b7c575817a060b1212535a1b4cb9f8f725d4ee2d
chit <cxl203@student.bham.ac.uk>
2023-03-27 22:44:07 +0100

    view all modules on all pages

--- askit/module_page/urls.py
+++ askit/module_page/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'module_page'
 urlpatterns = [
+    path('list/', views.view_module_list, name='module_list'),
     path('<str:mod>/', views.view_question_list, name='module'),
 ]

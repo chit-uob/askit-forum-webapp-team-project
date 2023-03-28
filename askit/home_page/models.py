e39833361a206f799b2493923630c67a56caaaf2
chit <cxl203@student.bham.ac.uk>
2023-03-28 17:01:10 +0100

    fix auto now

--- askit/home_page/models.py
+++ askit/home_page/models.py
@@ -17,7 +17,7 @@ class UserProfile(models.Model):
 class Module(models.Model):
     title = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=3000)
-    pub_date = models.DateTimeField(auto_now=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
     admins = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="module_admins")
     members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="module_members")
 
@@ -37,7 +37,7 @@ class Question(models.Model):
     explanation = models.CharField(max_length=3000)
     tried_what = models.CharField(max_length=500)
     summary = models.CharField(max_length=500)
-    pub_date = models.DateTimeField(auto_now=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, default="Unanswered")
     tags = models.ManyToManyField(Tag, related_name="question_tags")
     score = models.IntegerField(default=0)
@@ -53,7 +53,7 @@ class Answer(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.CharField(max_length=3000)
-    pub_date = models.DateTimeField(auto_now=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
     score = models.IntegerField(default=0)
     upvotes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="answer_upvotes")
     downvotes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="answer_downvotes")
@@ -75,7 +75,7 @@ class User(models.Model):
 class Notifications(models.Model):
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     detail = models.CharField(max_length=300)
-    date = models.DateTimeField(auto_now=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.detail
@@ -83,7 +83,7 @@ class Notifications(models.Model):
 class Comment(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     question = models.ForeignKey(Question, on_delete=models.SET_NULL, null=True)
-    pub_date = models.DateTimeField(auto_now=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
     content = models.CharField(max_length=3000, default="")
 
 

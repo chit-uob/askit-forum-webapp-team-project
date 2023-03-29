cbc5aaeb7e1c130c8a81082bc471b9706ce33496
chit <cxl203@student.bham.ac.uk>
2023-03-29 15:42:23 +0100

    test view_question response

--- askit/answer_questions/tests.py
+++ askit/answer_questions/tests.py
@@ -1,17 +1,27 @@
+import json
+
 from django.test import TestCase
 from django.contrib.auth.models import User
 from home_page.models import Question, Answer, Tag, Module, Comment
-from .views import check_upvote_or_downvote_question, check_upvote_or_downvote_answer
+from .views import check_upvote_or_downvote_question, check_upvote_or_downvote_answer, view_question
+from rest_framework.test import APIRequestFactory, force_authenticate
+
+
+def format_response_date(datetime_object):
+    return datetime_object.strftime("%Y-%m-%d") + "T" + datetime_object.strftime("%H:%M:%S.%f")[:-3] + "Z"
+
 
 # Create your tests here.
 
 class AnswerQuestionsTest(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.user = User.objects.create_user(username='testuser', password='123456789')
         self.user.save()
-        self.module = Module.objects.create(title='test module', description='test description')
+        self.module = Module.objects.create(title='testmodule', description='test description')
         self.module.save()
-        self.question = Question.objects.create(author=self.user, module=self.module, title='test question', explanation='test explanation', tried_what='test tried_what', summary='test summary')
+        self.question = Question.objects.create(author=self.user, module=self.module, title='test question',
+                                                explanation='test explanation', tried_what='test tried_what',
+                                                summary='test summary')
         self.question.save()
         self.answer = Answer.objects.create(author=self.user, question=self.question, content='test content')
         self.answer.save()
@@ -39,3 +49,22 @@ class AnswerQuestionsTest(TestCase):
         self.answer.upvotes.remove(self.user)
         self.answer.downvotes.add(self.user)
         self.assertEqual(check_upvote_or_downvote_answer(self.answer, self.user), "downvote")
+
+    def test_response_of_view_question(self):
+        factory = APIRequestFactory()
+        request = factory.get(f'/api/question/{self.question.id}/')
+        request.user = self.user
+        force_authenticate(request, user=self.user)
+        response = view_question(request, self.question.id)
+        response_json = json.loads(response.content)
+        self.assertEqual(response_json['question_id'], self.question.id)
+        expected_response = {"question_id": 1, "title": "test question", "author": "testuser", "module": "testmodule",
+                             "explanation": "test explanation", "tried_what": "test tried_what",
+                             "summary": "test summary", "pub_date": format_response_date(self.question.pub_date),
+                             "status": "Unanswered",
+                             "tags": [], "score": 0, "views": 1, "upvote_or_downvote": "none", "comment_list": [],
+                             "answer_list": [{"id": 1, "author": "testuser", "content": "test content",
+                                              "pub_date": format_response_date(self.answer.pub_date), "score": 0,
+                                              "is_solution": False,
+                                              "upvote_or_downvote": "none"}]}
+        self.assertEqual(response_json, expected_response)

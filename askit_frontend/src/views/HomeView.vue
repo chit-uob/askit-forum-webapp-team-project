1d6e0f50f44e35937f4f100392c0e3da85dd424a
Aliyu Suleiman <aliyusuleiman098@gmail.com>
2023-04-02 15:52:34 +0100

    Started making frontend for home page

--- askit_frontend/src/views/HomeView.vue
+++ askit_frontend/src/views/HomeView.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex min-h-screen justify-end py-10">
+    <div class="px-10 pr-64">
+      <h1 class="text-5xl font-bold">Welcome back</h1>
+      <h1 class="text-5xl font-bold">user</h1>
+    </div>
+
+      <div class="flex-row ">
+        <div class="grid grid-rows-3 text-right">
+          <h2 class="self-start text-3xl font-bold">Student name</h2>
+          <h2 class="self-center text-xl">University of Somewhere</h2>
+          <h2 class="self-end">email@student.xzy.ac.uk</h2>
+        </div>
+      </div>
+
+      <div class=" mt-2 mr-5 ml-5 rounded-2xl bg-white w-[100px] h-[100px] transition ease-in-out delay-75 hover:scale-[1.02] duration-150 hover:bg-[#F2FFFA] border-[0.12em] border-black" style="box-shadow: .13em .13em;"></div>
+
   </div>
 </template>
 
@@ -14,8 +27,6 @@ export default {
   components: {
     HelloWorld
   },
-  mounted() {
-    window.location.href = '/module/OSSP/';
-  }
+
 }
 </script>

5468509da6818c2c2ba501b6b42e9fbc10fe1ff3
Aditya Agarwal <adityaagarwal191@gmail.com>
2023-04-04 02:22:02 +0400

    option box for answers, but it doesnt fully work yet

--- askit_frontend/src/views/QuestionView.vue
+++ askit_frontend/src/views/QuestionView.vue
@@ -210,10 +210,65 @@
 
               <p v-if="answer.is_solution" class="mt-2 text-sm font-semibold text-green-500">Solution</p>
               <p class="text-lg">{{ answer.content }}</p>
-              <button v-if="user.username == answer.author" class="mt-3 mr-2 mb-2 rounded-lg bg-blue-600 px-4 text-sm font-medium text-white py-2 hover:bg-blue-500 focus:ring-4 focus:ring-blue-300"
+
+
+
+
+              <div>
+              <button v-if="user.username === answer.author"
+                class="mt-3 mr-2 mb-2 rounded-lg bg-blue-600 px-4 text-sm font-medium text-white py-2 hover:bg-blue-500 focus:ring-4 focus:ring-blue-300"
+                type="button"
+                @click="toggleAnswerDropdown">
+                <span>Options</span>
+              </button>
+
+              <div
+                class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100"
+                v-show="showDropdownAnswers" >
+                <div class="py-1">
+
+                  <button @click="editItem" 
+                  class="flex justify-start w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                    >
+                  <svg style="color: blue" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="mr-3 bi bi-pencil" viewBox="0 0 16 16"> 
+                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 
+                    1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 
+                    1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" fill="blue"></path> </svg>        
+                    Edit
+                  </button>
+
+                  <button v-on:click="deleteQuestion"
+                    class="flex justify-start w-full px-4 py-2 text-sm text-red-700 hover:bg-gray-100 hover:text-red-900">
+                    <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    Delete
+                  </button>
+
+                  <button v-on:click="muteNotifications"
+                    class="flex justify-start w-full px-4 py-2 text-sm text-black hover:bg-gray-100 hover:text-black">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="mr-3 bi bi-bell" viewBox="0 0 16 16"> 
+                      <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 
+                      1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 
+                      12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 
+                      1 13 6c0 .88.32 4.2 1.22 6z"/> </svg>
+                    Mute Notifications
+                  </button>
+                </div>
+              </div>
+              </div>
+
+
+
+
+
+
+
+              
+              <!-- <button v-if="user.username == answer.author" class="mt-3 mr-2 mb-2 rounded-lg bg-blue-600 px-4 text-sm font-medium text-white py-2 hover:bg-blue-500 focus:ring-4 focus:ring-blue-300"
           v-on:click="deleteAnswer(answer.id)">
               Delete Answer
-          </button>
+          </button> -->
           </div>
             </div>
           </div>
@@ -236,6 +291,7 @@ export default {
       showForm: false,
       showComments: false,
       showDropdown: false,
+      showDropdownAnswers:false,
       user:{},
 
       question: {
@@ -265,7 +321,8 @@ export default {
           this.question = response.data;
           this.answers = response.data.answer_list.sort((a, b) => b.score - a.score);
           this.all_comments = response.data.comment_list;
-          //this.question.Module.title = response.data.Module.title;
+          this.module = response.data.module;
+          this.question.Module.title = response.data.Module.title;
         })
         .catch((error) => {
           console.log(error);
@@ -293,6 +350,10 @@ export default {
   // },
     toggleDropdown() {
       this.showDropdown = !this.showDropdown;
+    },
+
+    toggleAnswerDropdown() {
+      this.showDropdownAnswers = !this.showDropdownAnswers;
     },
     addAnswer() {
       console.log(this.answerInput)

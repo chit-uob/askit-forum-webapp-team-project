4fbf5faa6bb78c0ac7414621e27a208ff2a08e49
Aymon Hussain <axh1220@student.bham.ac.uk>
2023-04-04 13:31:00 +0100

    added new frontend view

---
+++ askit_frontend/src/views/addMembersView.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>add Members</h1>
+  
+    <p>Course Title</p>
+    <p>Course Description</p>
+
+    <table>
+      <th>Full name</th>
+    </table>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: "addMembersView",
+  
+    data() {
+      return {}
+    },
+    methods: {},
+  }
+  
+  </script>
+  
+  <style scoped>
+  
+  </style>

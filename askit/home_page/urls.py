6cf8e72a09a54ec66d1dbed76ccea449b7ecfb49
Juni Katsu <jxk010@student.bham.ac.uk>
2023-04-12 10:36:37 +0100

    commit trail

---
+++ askit/home_page/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'home_page'
+urlpatterns = [
+    # path('', views.home_page, name='homePage'),
+    # path('about/', views.about_page, name='aboutPage'),
+    # path('contact/', views.contact_page, name='contactPage'),
+    # path('faq/', views.faq_page, name='faqPage'),
+    # path('privacy/', views.privacy_page, name='privacyPage'),
+    # path('terms/', views.terms_page, name='termsPage'),
+]
--- askit/home_page/views.py
+++ askit/home_page/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+@csrf_exempt
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def userInfo(request):
+    if request.method == 'POST':
+        post_data = json.loads(request.body)
+        userName = post_data['username']
+        first_name = post_data['first_name']
+        last_name = post_data['last_name']
+        user = User.objects.get(username=userName)
+        profile = UserProfile.objects.create(user=user,first_name=first_name,last_name=last_name)
+        profile.save()
+        return JsonResponse({"success": True})

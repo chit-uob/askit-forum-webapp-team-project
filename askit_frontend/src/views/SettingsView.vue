0d3dda809392129b5e50403c2d7cec0c01c0c7f2
chit <cxl203@student.bham.ac.uk>
2023-04-13 14:05:35 +0100

    button to send delete user request

--- askit_frontend/src/views/SettingsView.vue
+++ askit_frontend/src/views/SettingsView.vue
@@ -1,10 +1,22 @@
 <template>
 
+  <button @click="deleteUser">Test Delete User</button>
 </template>
 
 <script>
+import axiosClient from "@/views/axiosClient";
+
 export default {
-    name: "SettingsView"
+    name: "SettingsView",
+    methods: {
+        deleteUser() {
+            axiosClient.delete('/api/settings/delete_user/').then(response => {
+                console.log(response)
+            }).catch(error => {
+                console.log(error)
+            })
+        }
+    }
 }
 </script>
 

eb776076bc7e4e7973402868a8e552ae2915a216
Aditya Agarwal <adityaagarwal191@gmail.com>
2023-04-21 17:35:58 +0100

    fixed preferences accessibility box and button

--- askit_frontend/src/views/SettingsView.vue
+++ askit_frontend/src/views/SettingsView.vue
@@ -131,7 +131,7 @@
                         </label>
                         <div class="relative">
                             <select
-                                    class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                                    class="block appearance-none w-full bg-white border border-gray-400 focus:bg-gray-100 hover:bg-gray-100 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                                     id="theme-select">
                                 <option value="default">Default</option>
                                 <option value="dark">Dark</option>
@@ -147,7 +147,7 @@
                         </div>
                     </div>
                     <button
-                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            class="bg-blue-500 focus:bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                             type="submit">
                         Save Settings
                     </button>

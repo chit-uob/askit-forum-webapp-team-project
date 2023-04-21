712af5383b6d0045904dc247d4d26a90432b923a
chit <cxl203@student.bham.ac.uk>
2023-04-21 18:18:53 +0100

    home page dark mode

--- askit_frontend/src/App.vue
+++ askit_frontend/src/App.vue
@@ -1,87 +1,54 @@
 <template>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <nav v-if="enable" class="bg-teal-100 px-2  sm:px-4 border-b-[3px] border-black h-[68px]" id="nav-vue">
-        <div class="container mx-auto flex flex-wrap items-center justify-between">
+    <nav v-if="enable"
+         class="bg-teal-100 dark:bg-slate-900 px-2 sm:px-4 border-b-[3px] border-black dark:border-white h-[68px] text-black dark:text-white"
+         id="nav-vue">
+        <div class="container mx-auto flex flex-wrap items-center justify-between h-full">
             <router-link to="/" class="pl-4 transition focus:ring-4 focus:outline-none focus:ring-blue-400">
                 <span class="text-2xl font-bold">ASK.IT</span>
             </router-link>
             <form class="w-1/2" @submit.prevent="search">
                 <label for="default-search"
-                       class="sr-only mb-2 text-sm font-medium text-gray-900">Search</label>
+                       class="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-gray-100">Search</label>
                 <div class="relative">
                     <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                         <i class="fa fa-search" aria-hidden="true"></i>
                     </div>
                     <input type="search" id="default-search"
-                           class="transition focus:ring-4 focus:outline-none block w-full h-[45px] rounded-lg border-2 border-black bg-white p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                           class="transition focus:ring-4 focus:outline-none block w-full h-[45px] rounded-lg border-2 border-black dark:border-white bg-white dark:bg-black p-4 pl-10 text-sm text-gray-900 dark:text-gray-100 focus:border-blue-500 dark:focus:border-blue-300 focus:ring-blue-500 dark:focus:ring-blue-300"
                            placeholder="Search questions" v-model="searchTerm" required>
                     <button type="submit"
-                            class="transition absolute rounded-lg top-1 bg-blue-700 px-4  text-sm font-medium text-white h-[33px] right-2.5 bottom-1.5 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-pink-400">
+                            class="transition absolute rounded-lg bg-blue-700 px-4 text-sm font-medium text-white h-[33px] right-2.5 bottom-1.5 hover:bg-blue-800 dark:hover:bg-blue-200 focus:outline-none focus:ring-4 focus:ring-pink-400">
                         Search
                     </button>
                 </div>
             </form>
-            <button type="button"
-                    class="ml-3 inline-flex items-center rounded-lg bg-teal-100 p-2 text-sm text-gray-500 outline-1 outline-teal-300 hover:bg-teal-300 focus:outline-none focus:ring-2 focus:ring-blue-400 md:hidden"
-                    aria-controls="navbar-default" aria-expanded="false"
-                    v-on:click="showNav">
-                <span class="sr-only">Open main menu</span>
-                <i class="fa fa-bars" aria-hidden="true"></i>
-            </button>
-            <div class="z-20 mt-2 w-full flex-grow p-4 text-black" :class="this.showMobileNav ? 'block' : 'hidden'"
-                 id="mobile-nav">
-                <ul>
-                    <li><i class="pr-1 fa fa-bell-o" aria-hidden="true"></i>Notifications</li>
-                    <li><i class="pr-1 fa fa-user" aria-hidden="true"></i>Profile</li>
-                    <li><i class="pr-1 fa fa-question-circle" aria-hidden="true"></i>Help</li>
-                    <li><i class="pr-1 fa fa-cog" aria-hidden="true"></i>Settings</li>
-                </ul>
-            </div>
-            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <div class="w-auto" id="navbar-default">
                 <ul class="mt-4 flex flex-col p-4 md:space-x-8 md:mt-0 md:flex-row md:border-0 md:text-sm md:font-medium">
-                    <!--                    <li>-->
-                    <!--                        <a href="#"-->
-                    <!--                           class="transition focus:ring-4 focus:outline-none focus:ring-blue-400 block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"-->
-                    <!--                           aria-current="page"><i class="fa fa-bell-o scale-150" aria-hidden="true"></i></a>-->
-                    <!--                    </li>-->
-                    <!--                    <li>-->
-                    <!--                        <a href="#"-->
-                    <!--                           class="transition focus:ring-4 focus:outline-none focus:ring-blue-400 block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"><i-->
-                    <!--                                class="fa fa-user scale-150" aria-hidden="true"></i></a>-->
-                    <!--                    </li>-->
-                    <!--                    <li>-->
-                    <!--                        <a href="#"-->
-                    <!--                           class="transition focus:ring-4 focus:outline-none focus:ring-blue-400 block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"><i-->
-                    <!--                                class="fa fa-question-circle scale-150" aria-hidden="true"></i></a>-->
-                    <!--                    </li>-->
                     <li>
                         <a href="/settings/"
-                           class="transition focus:ring-4 focus:outline-none focus:ring-blue-400 block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"><i
+                           class="transition focus:ring-4 focus:outline-none focus:ring-blue-400 block rounded py-2 pr-4 pl-3 text-gray-700 dark:text-gray-200 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"><i
                                 class="fa fa-cog scale-150 pr-2" aria-hidden="true"></i>Settings</a>
                     </li>
                 </ul>
             </div>
-            <!--            <button type="button" v-on:click="logout"-->
-            <!--                    class="transition focus:ring-4 focus:outline-none focus:ring-blue-400">-->
-            <!--                Logout-->
-            <!--            </button>-->
         </div>
     </nav>
-    <div class="absolute md:hidden">
+    <div class="absolute md:hidden text-black dark:text-white">
         <button class="bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded" @click="toggleModule"
                 v-if="enable">
             Select module
         </button>
     </div>
-    <div class="flex">
-        <div class="flex-col justify-items-center bg-cyan-50 w-[175px] md:block border-r-[3px] border-black"
+    <div class="flex text-black dark:text-white">
+        <div class="flex-col justify-items-center bg-cyan-50 dark:bg-slate-900 w-[175px] md:block border-r-[3px] border-black dark:border-white"
              v-if="enable" :class="{ hidden: !showModule }">
             <h1 class="ml-5 p-3 text-lg font-bold">Modules</h1>
             <a v-for="module in modules"
-               class="shadow-[4px_4px_0px_0px_#000000] transition hover:translate-x-1 hover:bg-[#fff6fe] focus:ring-4 focus:outline-none focus:ring-blue-400 mx-5 my-4 rounded-2xl border-2  border-black bg-white flex h-[100px] w-[100px] justify-center"
+               class="shadow-[4px_4px_0px_0px_#000000] transition hover:translate-x-1 hover:bg-[#fff6fe] dark:hover:bg-[#240020] focus:ring-4 focus:outline-none focus:ring-blue-400 mx-5 my-4 rounded-2xl border-2  border-black dark:border-white bg-white dark:bg-black flex h-[100px] w-[100px] justify-center"
                :href="'/module/' + module.title">
-                <div class="justify-center flex w-full m-2 rounded-[0.6rem] border-2 border-black border-dashed">
+                <div class="justify-center flex w-full m-2 rounded-[0.6rem] border-2 border-black dark:border-white border-dashed">
                     <p class="inline-block place-self-center text-xl tracking-[1px]">{{ module.title }}</p>
                 </div>
             </a>
@@ -91,12 +58,12 @@
         </div>
     </div>
 
-    <footer class="rounded-lg bg-sky-100 p-4 shadow md:flex md:items-center md:justify-between md:p-6">
-    <span class="text-sm text-gray-500 sm:text-center">© 2023 <a href="#" class="hover:underline">TeamAI55</a>. All Rights Reserved.
+    <footer class="rounded-lg bg-sky-100 dark:bg-sky-800 p-4 shadow md:flex md:items-center md:justify-between md:p-6 text-black dark:text-white text-sm">
+    <span class=" sm:text-center">© 2023 <a href="#" class="hover:underline">TeamAI55</a>. All Rights Reserved.
     </span>
-        <span class="text-sm text-gray-500 sm:text-center">Alpha Project Disclaimer This server is provided by the School of Computer Science at the University of Birmingham to allow users to provide feedback on software developed by students as part of an assignment. While we take reasonable precautions, we cannot guarantee the security of the data entered into the system. Do NOT enter any real personal data (e.g., financial information or otherwise) into the system. The assignment runs until May 31st 2023, at which time the server and all associated data will be destroyed.
+        <span class="sm:text-center">Alpha Project Disclaimer This server is provided by the School of Computer Science at the University of Birmingham to allow users to provide feedback on software developed by students as part of an assignment. While we take reasonable precautions, we cannot guarantee the security of the data entered into the system. Do NOT enter any real personal data (e.g., financial information or otherwise) into the system. The assignment runs until May 31st 2023, at which time the server and all associated data will be destroyed.
     </span>
-        <ul class="mt-3 flex flex-wrap items-center text-sm text-gray-500 sm:mt-0">
+        <ul class="mt-3 flex flex-wrap items-center sm:mt-0">
             <li>
                 <a target="_blank" href="/privacy" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
             </li>

218aceeab7be99c0dac0d3cd4c34a3cf0193f5bb
Aditya Agarwal <adityaagarwal191@gmail.com>
2023-04-22 17:52:16 +0100

    dark mode search view

--- askit_frontend/src/views/SearchView.vue
+++ askit_frontend/src/views/SearchView.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="flex min-h-screen">
+    <div class=" dark:bg-gray-900 flex min-h-screen">
         <div class="w-2/3">
             <div class="inline-flex w-full justify-between p-10">
                 <div>
@@ -24,49 +24,48 @@
             </div>
             <a v-for="question in questions" :key="question.id"
                :href="`/question/${question.id}`"
-               class="transition group focus:ring-4 focus:outline-none focus:ring-blue-400 shadow-[5px_5px_0px_0px_#000000]  hover:translate-x-1 sm:grid hidden grid-cols-[100px_1fr_90px] md:mx-10 mb-[8px] box-content min-h-[90px] rounded-2xl  bg-white hover:bg-[#f2fcff] border-[0.24em] border-black ">
-                <div class="grid grid-rows-3 text-right  text-xs font-medium pr-2 border-r-[0.16em] border-black my-3  object-fill box-content">
-                    <div class=" self-start ">{{ question.score }} votes</div>
-                    <div class=" self-center ">{{ question.num_answers }} answers</div>
-                    <span class=" self-end  ">{{ question.views }} views</span>
-                </div>
+               class="hidden focus:ring-4 focus:outline-none dark:bg-gray-800 focus:ring-blue-400 shadow-[5px_5px_0px_0px_#000000] transition hover:translate-x-1 duration-300 sm:grid grid-cols-[100px_1fr_90px] md:mx-10 mb-[8px] box-content min-h-[90px] rounded-2xl bg-white hover:bg-[#f2fcff] border-[0.24em] border-black ">
+                    <div class="grid grid-rows-3 text-right  text-xs  font-medium pr-2 border-r-[0.16em]  border-black my-3  object-fill box-content">
+                        <div class=" self-start ">{{ question.score }} votes</div>
+                        <div class=" self-center ">{{ question.num_answers }} answers</div>
+                        <span class=" self-end  ">{{ question.views }} views</span>
+                    </div>
 
-                <div class="grid grid-rows-3 pl-2 text-xs font-medium py-3 pr-1  box-content object-fill">
-                    <div class=" truncate self-start text-base leading-[1.15] text-blue-500 hover:underline hover:text-blue-400">
-                        {{ question.title }}
-                    </div>
-                    <div class=" self-center truncate ">Asked by <span v-if="question.author"
-                                                                       class="text-blue-500 hover:underline hover:text-blue-400">{{
-                        question.author
-                        }}</span><span v-if="!(question.author)" class="">Anonymous</span></div>
-                    <div v-if="(question.tags[0] != '') && (question.tags.length != 0)"
-                         class="flex overflow-hidden">
-                        <div v-for="tag in question.tags"
-                             class="  whitespace-nowrap self-end mr-[2px] text-blue-500 hover:underline hover:text-blue-400">
-                            [{{ addTagToSet(tag) }}]
+                    <div class="grid grid-rows-3 pl-2 text-xs font-medium py-3 pr-1  box-content object-fill">
+                        <div class=" truncate self-start text-base leading-[1.15]  dark:text-blue-400  dark:hover:text-blue-300 text-blue-500 hover:underline hover:text-blue-400">
+                            {{ question.title }}
+                        </div>
+                        <div class=" truncate self-center ">Asked by <span v-if="question.author"
+                                                                           class="dark:text-blue-400  dark:hover:text-blue-300 text-blue-500 hover:underline hover:text-blue-400">{{
+                            question.author
+                            }}</span><span v-if="!(question.author)" class="">Anonymous</span></div>
+                        <div v-if="(question.tags[0] != '') && (question.tags.length != 0)"
+                             class="flex overflow-hidden">
+                            <div v-for="tag in question.tags"
+                                 class=" whitespace-nowrap self-end mr-[2px] dark:text-blue-400  dark:hover:text-blue-300 text-blue-500 hover:underline hover:text-blue-400">
+                                [{{ tag }}]
+                            </div>
+                        </div>
+                        <div v-if="(question.tags[0] == '') || (question.tags.length == 0)"
+                             class="flex overflow-hidden">
+                            <div class=" self-end mr-[2px]">No tags! (<span class=" text-cyan-500">╥</span>_<span
+                                    class=" text-cyan-500">╥</span>)
+                            </div>
                         </div>
                     </div>
-                    <div v-if="(question.tags[0] == '') || (question.tags.length == 0)"
-                         class="flex overflow-hidden">
-                        <div class=" self-end mr-[2px]">No tags! (<span class=" text-cyan-500">╥</span>_<span
-                                class=" text-cyan-500">╥</span>)
-                        </div>
-                    </div>
-                </div>
 
-                <div class="grid bg-lime-300 rounded-r-[13px] rounded-bl-2xl  box-content">
-                    <div class="  place-self-center py-2 px-[9px] border-[.1em] border-black border-dashed border-spacing-5 rounded-r-md rounded-bl-md">
-                        <div class=" text-center leading-[0.9] text-[38px] font-semibold ">
-                            {{ formatDay(question.pub_date) }}
-                        </div>
-                        <div class=" text-center text-[16px] font-medium leading-none ">
-                            {{ formatMonthYear(question.pub_date).toLowerCase() }}
+                    <div class=" bg-lime-300 dark:bg-blue-600 rounded-r-[13px] rounded-bl-2xl grid box-content">
+                        <div class="  place-self-center py-2 px-[9px] border-[0.1em] border-black border-dashed border-spacing-5 rounded-r-md rounded-bl-md">
+                            <div class=" text-center leading-[0.9] text-[38px] font-semibold ">
+                                {{ formatDay(question.pub_date) }}
+                            </div>
+                            <div class=" text-center text-[16px] font-medium leading-none ">
+                                {{ formatMonthYear(question.pub_date).toLowerCase() }}
+                            </div>
                         </div>
                     </div>
+                </a>
                 </div>
-
-            </a>
-        </div>
     </div>
 
 </template>

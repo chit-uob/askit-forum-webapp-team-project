9cf52c84a1876dd6416d613293da604d93f3c9ee
Aymon Hussain <axh1220@student.bham.ac.uk>
2023-04-23 21:37:27 +0100

    admin pages dark mode added

--- askit_frontend/src/views/addMembersView.vue
+++ askit_frontend/src/views/addMembersView.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="m-10">
+<div class="p-10 dark:bg-slate-900">
   <div class=" text-4xl mb-3">Add members to {{ $route.params.mod }}</div>
   <div>
     <div class="text-xl">All users</div>
-    <div class="rounded border-2 border-black p-2" style="box-shadow: .2em .2em black;">
-      <div class=" grid-cols-4 font-medium  grid border-b-2 border-black" >
+    <div class=" dark:bg-gray-800 rounded border-2 dark:border-white border-black p-2 shadow-[3px_3px_0px_0px_#000000] dark:shadow-[3px_3px_0px_0px_#ffffff]">
+      <div class="  grid-cols-4 font-medium  grid border-b-2 border-black" >
         <div>Full name</div>
         <div>Email</div>
         <div class="text-center">Member</div>
@@ -20,15 +20,15 @@
       </div>
     </div>   
     <div class="flex"> 
-      <button v-on:click="update" type="button" class="shadow-[3px_3px_0px_0px_#000000] focus:ring-4 focus:outline-none focus:ring-pink-400 p-2 my-2 rounded border-black border-2 bg-blue-500 text-white hover:bg-blue-600 transition" >Update</button>
+      <button v-on:click="update" type="button" class="dark:shadow-[3px_3px_0px_0px_#ffffff] dark:border-0 shadow-[3px_3px_0px_0px_#000000] focus:ring-4 focus:outline-none focus:ring-pink-400 p-2 my-2 rounded border-black border-2 bg-blue-500 text-white hover:bg-blue-600 transition" >Update</button>
       <div v-if="success" class="mx-5 self-center text-lg text-green-700 font-medium">Success!</div>
     </div>
     <div class="my-4">
       <label class="text-lg font-medium">Add/Remove members by email</label>
       <input type="text" placeholder="Add multiple emails seperated by commas e.g: (person1@email.com,person2@email.com)" id="default-input" v-model="usersCSV" class="transition shadow-[3px_3px_0px_0px_#000000] bg-gray-50 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-400 focus:border-blue-500 block w-full p-2.5 border-2 border-black" >
       <div class="flex">
-        <button v-on:click="addMembers(false)" type="button" class="shadow-[3px_3px_0px_0px_#000000] focus:ring-4 focus:outline-none focus:ring-pink-400 p-2 my-2 mr-2 rounded border-black border-2 bg-blue-500 text-white hover:bg-blue-600 transition" >Add</button>
-        <button v-on:click="addMembers(true)" type="button" class="shadow-[3px_3px_0px_0px_#000000] focus:ring-4 focus:outline-none focus:ring-pink-400 p-2 my-2 rounded border-black border-2 bg-red-700 text-white hover:bg-red-800 transition" >Remove</button>
+        <button v-on:click="addMembers(false)" type="button" class="shadow-[3px_3px_0px_0px_#000000] dark:shadow-[3px_3px_0px_0px_#ffffff] dark:border-0 focus:ring-4 focus:outline-none focus:ring-pink-400 p-2 my-2 mr-2 rounded border-black border-2 bg-blue-500 text-white hover:bg-blue-600 transition" >Add</button>
+        <button v-on:click="addMembers(true)" type="button" class="shadow-[3px_3px_0px_0px_#000000] dark:shadow-[3px_3px_0px_0px_#ffffff] dark:border-0 focus:ring-4 focus:outline-none focus:ring-pink-400 p-2 my-2 rounded border-black border-2 bg-red-700 text-white hover:bg-red-800 transition" >Remove</button>
         <div v-if="membersAddShow" class="mx-5 self-center text-lg font-medium">{{ successFromMembersAdd }} success, {{ failFromMembersAdd }} failed <span class="text-sm text-gray-500">(refresh the page to see changes)</span></div>
       </div>
     </div>
@@ -36,8 +36,8 @@
       <label class="text-lg font-medium">Add/Remove admins by email</label>
       <input type="text" placeholder="Add multiple emails seperated by commas e.g: (person1@email.com,person2@email.com)" id="default-input" v-model="adminsCSV" class=" transition shadow-[3px_3px_0px_0px_#000000] bg-gray-50 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-400 focus:border-blue-500 block w-full p-2.5 border-2 border-black" >
       <div class="flex">
-        <button v-on:click="addAdmins(false)" type="button" class="shadow-[3px_3px_0px_0px_#000000] focus:ring-4 focus:outline-none focus:ring-pink-400 p-2 my-2 mr-2 rounded border-black border-2 bg-blue-500 text-white hover:bg-blue-600 transition" >Add</button>
-        <button v-on:click="addAdmins(true)" type="button" class="shadow-[3px_3px_0px_0px_#000000] focus:ring-4 focus:outline-none focus:ring-pink-400 p-2 my-2 rounded border-black border-2 bg-red-700 text-white hover:bg-red-800 transition" >Remove</button>
+        <button v-on:click="addAdmins(false)" type="button" class="shadow-[3px_3px_0px_0px_#000000] dark:shadow-[3px_3px_0px_0px_#ffffff] dark:border-0 focus:ring-4 focus:outline-none focus:ring-pink-400 p-2 my-2 mr-2 rounded border-black border-2 bg-blue-500 text-white hover:bg-blue-600 transition" >Add</button>
+        <button v-on:click="addAdmins(true)" type="button" class="shadow-[3px_3px_0px_0px_#000000] dark:shadow-[3px_3px_0px_0px_#ffffff] dark:border-0 focus:ring-4 focus:outline-none focus:ring-pink-400 p-2 my-2 rounded border-black border-2 bg-red-700 text-white hover:bg-red-800 transition" >Remove</button>
         <div v-if="adminsAddShow" class="mx-5 self-center text-lg font-medium">{{ successFromAdminsAdd }} success, {{ failFromAdminsAdd }} failed <span class="text-sm text-gray-500">(refresh the page to see changes)</span></div>
       </div>
     </div>
@@ -90,10 +90,10 @@
       },
       colour(index){
         if(index%2==0){
-          return "bg-gray-white"
+          return "bg-gray-white dark:bg-gray-800"
         }
         else{
-          return "bg-gray-200"
+          return "bg-gray-200 dark:bg-gray-700"
         }
       },
       update(){

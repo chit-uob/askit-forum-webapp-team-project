91c12d8ff0264b62e81c2d8d1c32c78f2a69276b
Aditya Agarwal <adityaagarwal191@gmail.com>
2023-04-24 13:29:31 +0100

    fixed delete question bug

--- askit/answer_questions/urls.py
+++ askit/answer_questions/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('<int:question_id>/downvote/', views.downvote_question, name='downvote'),
     path('<int:question_id>/answer/<int:answer_id>/upvote/', views.upvote_answer, name='upvote_answer'),
     path('<int:question_id>/answer/<int:answer_id>/downvote/', views.downvote_answer, name='downvote_answer'),
+    path('<int:question_id>/delete/',views.delete_question, name='delete_question'),
     path('RESET/', views.set_up_test_database, name='set_up_test_database'),
 ]
